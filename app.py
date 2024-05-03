4197aa58beb65d7ede52459029ac886816efbf7e
Ali Badran <ali.badran53@yahoo.com>
2024-05-04 00:45:01 +0400

    fixing repo structure

---
+++ app.py
@@ -0,0 +1,281 @@
+import traceback
+
+import requests
+from flask import Flask, render_template
+from flask import request, jsonify
+from flask_cors import CORS
+from ee_utils import *
+from flask import Flask, jsonify
+import ee
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+CORS(app)
+ee.Initialize()
+
+
+MOLAR_VOLUME_AT_STP = 22.414  # Molar volume of gas at STP in liters
+MOLAR_MASSES = {  # Molar masses in g/mol
+    "NO2": 46.0055,
+    "SO2": 64.066,
+    "O3": 48.00,
+    "CO": 28.01,
+}
+STANDARD_PRESSURE = 1013.25  # in hPa
+STANDARD_TEMPERATURE = 273.15  # in Kelvin
+
+
+def umol_per_m2_to_ppb(umol_per_m2, gas):
+    """Converts concentration from µmol/m² to ppb."""
+    # Using molar mass to convert µmol/m² to µg/m², then to ppb
+    micrograms_per_m2 = umol_per_m2 * MOLAR_MASSES[gas]
+    ppb = micrograms_per_m2 / (MOLAR_VOLUME_AT_STP * 1e3)  # Convert µg/m³ to ppb
+    return ppb
+
+
+def mol_per_m2_to_ppm(mol_per_m2, gas):
+    """Converts concentration from mol/m² to ppm."""
+    # Using molar mass to convert mol/m² to g/m², then to ppm
+    grams_per_m2 = mol_per_m2 * MOLAR_MASSES[gas]
+    ppm = grams_per_m2 / (MOLAR_VOLUME_AT_STP * 1e6)  # Convert g/m³ to ppm
+    return ppm
+
+
+def get_date_range():
+    today = datetime.now()
+    last_week = today - timedelta(days=7)
+    return last_week.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d')
+
+
+def calculate_aqi(Cp, breakpoints):
+    for breakpoint in breakpoints:
+        C_low, C_high, AQI_low, AQI_high = breakpoint
+        if C_low <= Cp <= C_high:
+            return ((AQI_high - AQI_low) / (C_high - C_low)) * (Cp - C_low) + AQI_low
+    return
+
+
+breakpoints = {
+    # (lower limit, upper limit, lower limit of quality, upper limit of quality). NO2, SO2 1 hour exposure in ppb, CO, O3 8hr exp ppm)
+    "O3": [(0, 0.054, 0, 50), (0.055, 0.070, 51, 100), (0.071, 0.085, 101, 150), (0.086, 0.105, 151, 200),
+           (0.106, 0.200, 201, 300)],
+    "NO2": [(0, 53, 0, 50), (54, 100, 51, 100), (101, 360, 101, 150), (361, 649, 151, 200), (650, 1249, 201, 300),
+            (1250, 2049, 301, 500)],
+    "SO2": [(0, 35, 0, 50), (36, 75, 51, 100), (76, 185, 101, 150), (186, 304, 151, 200), (305, 604, 201, 300),
+            (605, 1004, 301, 500)],
+    "CO": [(0, 4.4, 0, 50), (4.5, 9.4, 51, 100), (9.5, 12.4, 101, 150), (12.5, 15.4, 151, 200), (15.5, 30.4, 201, 300),
+           (30.5, 50.4, 301, 500)],
+    "PM25": [(0, 12, 0, 50), (12.1, 35.4, 51, 100), (35.5, 55.4, 101, 150), (55.5, 150.4, 151, 200),
+             (150.5, 250.4, 201, 300), (250.5, 500.4, 301, 500)],
+    "PM10": [(0, 54, 0, 50), (55, 154, 51, 100), (155, 254, 101, 150), (255, 354, 151, 200), (355, 424, 201, 300),
+             (425, 604, 300, 500)]
+
+}
+
+
+@app.route('/api/openaq', methods=['GET'])
+def get_openaq_data():
+    latitude = request.args.get('lat')
+    longitude = request.args.get('lon')
+    date_from = request.args.get('date_from')
+    date_to = request.args.get('date_to')
+    limit = request.args.get('limit', 10000)
+
+    url = f"https://api.openaq.org/v2/measurements?coordinates={latitude},{longitude}&radius=1000&date_from={date_from}&date_to={date_to}&limit={limit}"
+    headers = {"X-API-Key": "64b0bb7bcecc37a2b4bee22f281748852e3055e3062d0d6fde9fce62977ff12b"}
+
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
+
+
+@app.route('/gas-info', methods=['POST'])
+def gas_info():
+    request_data = request.get_json()
+    latitude = float(request_data['latitude'])
+    longitude = float(request_data['longitude'])
+    start_date, end_date = get_date_range()
+
+    point = ee.Geometry.Point([longitude, latitude])
+    gases = {
+        "NO2": {"id": "COPERNICUS/S5P/NRTI/L3_NO2", "band": "NO2_column_number_density", "scale": 1e9,
+                "unit": "µmol/m²"},
+        "SO2": {"id": "COPERNICUS/S5P/NRTI/L3_SO2", "band": "SO2_column_number_density", "scale": 1e9,
+                "unit": "µmol/m²"},
+        "CO": {"id": "COPERNICUS/S5P/NRTI/L3_CO", "band": "CO_column_number_density", "scale": 1e9, "unit": "mol/m²"},
+        "O3": {"id": "COPERNICUS/S5P/NRTI/L3_O3", "band": "O3_column_number_density", "scale": 1e9, "unit": "µmol/m²"},
+        "CH4": {"id": "COPERNICUS/S5P/OFFL/L3_CH4", "band": "CH4_column_volume_mixing_ratio_dry_air", "scale": 1,
+                "unit": "ppb"},
+        "HCHO": {"id": "COPERNICUS/S5P/NRTI/L3_HCHO", "band": "tropospheric_HCHO_column_number_density", "scale": 1e9,
+                 "unit": "mol/m²"}
+    }
+
+    response_data = {}
+    for gas, info in gases.items():
+        collection = ee.ImageCollection(info["id"]) \
+            .select(info["band"]) \
+            .filterDate(start_date, end_date)
+
+        mean_img = collection.mean()
+        value = mean_img.reduceRegion(ee.Reducer.mean(), point, scale=1000).get(info["band"]).getInfo()
+
+        if value is not None:
+            scaled_value = value * info["scale"]
+            if scaled_value < 0:
+                formatted_value = "Below Detection Limit"
+            else:
+                formatted_value = f"{scaled_value:.2e} {info['unit']}"  # Adjust formatting based on the scale
+        else:
+            formatted_value = "Data not available"
+
+        response_data[gas] = formatted_value
+
+    return jsonify(response_data)
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/trends')
+def trends():
+    return render_template('trends.html');
+
+
+ee.Initialize()
+
+
+@app.route('/test', methods=['POST'])
+def test():
+    request_data = request.get_json()
+    print(request_data);
+    selected_gas = request_data['gas']
+    start_date = request_data['startDate']
+    end_date = request_data['endDate']
+
+    # Dictionary to map gases to their corresponding image collection IDs
+    gas_to_collection = {
+        "SO2": "COPERNICUS/S5P/NRTI/L3_SO2",
+        "NO2": "COPERNICUS/S5P/NRTI/L3_NO2",
+        "CO": "COPERNICUS/S5P/NRTI/L3_CO",
+        "HCHO": "COPERNICUS/S5P/NRTI/L3_HCHO",
+        "O3": "COPERNICUS/S5P/NRTI/L3_O3",
+        "CH4": "COPERNICUS/S5P/OFFL/L3_CH4",
+    }
+
+    gas_viz_params = {
+        "SO2": {'bands': ['SO2_column_number_density'], 'min': 0, 'max': 0.0005,
+                'palette': ['black', 'blue', 'purple', 'cyan', 'green', 'yellow', 'red']},
+        "NO2": {'bands': ['NO2_column_number_density'], 'min': 0, 'max': 0.0002,
+                'palette': ['purple', 'blue', 'green', 'yellow', 'red']},
+        "CO": {'bands': ['CO_column_number_density'], 'min': 0, 'max': 0.05,
+               'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+        "HCHO": {'bands': ['tropospheric_HCHO_column_number_density'], 'min': 0, 'max': 0.0003,
+                 'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+        "O3": {'bands': ['O3_column_number_density'], 'min': 0.12, 'max': 0.15,
+               'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+        "CH4": {'bands': ['CH4_column_volume_mixing_ratio_dry_air'], 'min': 1750, 'max': 1900,
+                'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+    }
+
+    # Get the image collection ID from the dictionary
+    collection_id = gas_to_collection[selected_gas]
+    band_viz = gas_viz_params[selected_gas]
+
+    print("Start Date:", start_date, "End Date:", end_date)
+
+    # Fetch the collection and filter for the UAE
+    countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017')
+    uae = countries.filter(ee.Filter.eq('country_na', 'United Arab Emirates'))
+    uae_boundaries = uae.first().geometry()
+
+    collection = ee.ImageCollection(collection_id) \
+        .select(band_viz['bands']) \
+        .filterDate(start_date, end_date) \
+        .mean() \
+        .clip(uae_boundaries)
+
+    url = image_to_map_id(collection, band_viz)
+    return jsonify(url), 200
+
+
+#Dictionary for gasses and their respective collections and bands
+gas_mapping = {
+    "SO2": {
+        'collection_name': "COPERNICUS/S5P/NRTI/L3_SO2",
+        'index_name': 'SO2_column_number_density'
+    },
+    "NO2": {
+        'collection_name': "COPERNICUS/S5P/NRTI/L3_NO2",
+        'index_name': 'NO2_column_number_density'
+    },
+    "CO": {
+        'collection_name': "COPERNICUS/S5P/NRTI/L3_CO",
+        'index_name': 'CO_column_number_density'
+    },
+
+    "HCHO": {
+        'collection_name': "COPERNICUS/S5P/NRTI/L3_HCHO",
+        'index_name':'tropospheric_HCHO_column_number_density'
+    },
+     "O3": {
+        'collection_name': "COPERNICUS/S5P/NRTI/L3_O3",
+        'index_name': 'O3_column_number_density'
+    },
+     "CH4": {
+        'collection_name': "COPERNICUS/S5P/OFFL/L3_CH4",
+        'index_name': 'CH4_column_volume_mixing_ratio_dry_air'
+    },
+}
+
+#collection to filter the data
+countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017')
+uae = countries.filter(ee.Filter.eq('country_na', 'United Arab Emirates'))
+uae_boundaries = uae.first().geometry()
+
+preset_reducer = 'mean' # reducer for the data
+preset_scale = 30
+
+
+@app.route('/timeSeriesIndex', methods=['POST'])
+def time_series_index():
+    try:
+        raw_data = request.data  # This will capture raw bytes sent to the endpoint
+        print("Raw data received:", raw_data)
+
+        request_json = request.get_json()
+        print('Received JSON:', request_json)
+
+        if request_json:
+            gas_selection = request_json.get('gas', None)
+            start_date = request_json.get('startDate', None)
+            end_date = request_json.get('endDate', None)
+
+            # Get the collection name and index name based on the gas selection
+            gas_info = gas_mapping.get(gas_selection, None)
+
+            if gas_info and start_date and end_date:
+                values = get_time_series_by_collection_and_index(gas_info['collection_name'],
+                                                                  gas_info['index_name'],
+                                                                  preset_scale,
+                                                                  uae_boundaries,
+                                                                  start_date,
+                                                                  end_date,
+                                                                  preset_reducer)
+            else:
+                raise Exception("Missing required parameters")
+        else:
+            raise Exception("Invalid request payload")
+    except Exception as e:
+        values = {
+            'errMsg': str(e)
+        }
+
+    return jsonify(values), 200
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/earthenginedemo.js
@@ -0,0 +1,261 @@
+let currentLayer;
+let currentChart;
+let bandCollector;
+
+$(function () {
+  // Initial map load
+  loadMap("map", ol.proj.transform([54.0000, 24.0000], 'EPSG:4326', 'EPSG:3857'), 7);
+
+  // Call test function with the default selected gas when the page loads
+  const defaultGas = $('#gas-select').val();
+  test(defaultGas); // Note: Ensure this is the correct usage as per your application's logic
+  timeSeriesIndex(defaultGas)
+  // Dropdown selection change handler
+  $('#gas-select').change(function() {
+    var selectedGas = $(this).val();
+    test(selectedGas);
+  });
+});
+
+const gasVizParams = {
+  "SO2": {'bands': ['SO2_column_number_density'], 'min': 0, 'max': 0.0005,
+                'palette': ['black', 'blue', 'purple', 'cyan', 'green', 'yellow', 'red']},
+        "NO2": {'bands': ['NO2_column_number_density'], 'min': 0, 'max': 0.0002,
+                'palette': ['purple', 'blue', 'green', 'yellow', 'red']},
+        "CO": {'bands': ['CO_column_number_density'], 'min': 0, 'max': 0.05,
+               'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+        "HCHO": {'bands': ['tropospheric_HCHO_column_number_density'], 'min': 0, 'max': 0.0001,
+                 'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+        "O3": {'bands': ['O3_column_number_density'], 'min': 0, 'max': 0.0003,
+               'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+        "CH4": {'bands': ['CH4_column_volume_mixing_ratio_dry_air'], 'min': 1750, 'max': 1900,
+                'palette': ['black', 'blue', 'green', 'yellow', 'red']},
+};
+
+function test(selectedGas) {
+   const startDate = $('#start-date').val();
+   const endDate = $('#end-date').val();
+
+  $.ajax({
+    url: api_url + "test", // Make sure api_url is correctly defined
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({ gas: selectedGas, startDate: startDate, endDate: endDate }),
+    success: function(data) {
+      if (currentLayer) {
+        map.removeLayer(currentLayer); // Remove the existing layer
+      }
+      currentLayer = addMapLayer(data.url); // Add the new layer and keep track of it
+      const vizParams = gasVizParams[selectedGas];
+      updateLegend(vizParams.min, vizParams.max, vizParams.palette);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.error('Error fetching data:', textStatus, errorThrown);
+    }
+  });
+}
+
+$('#gas-select').change(function() {
+    updateDatePickerRange($(this).val());
+    test($(this).val()); // Ensure test is called with the current gas as parameter
+    timeSeriesIndex($(this).val()) // ---> ?
+});
+
+function addMapLayer(url) {
+  const newLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: url
+    }),
+    opacity: 0.6
+  });
+  map.addLayer(newLayer);
+  return newLayer; // Return the new layer for tracking
+}
+
+function updateLegend(minValue, maxValue, palette) {
+  // Clear the existing legend content
+  var legendCanvas = document.getElementById('legend-canvas');
+  var ctx = legendCanvas.getContext('2d');
+  ctx.clearRect(0, 0, legendCanvas.width, legendCanvas.height);
+
+  // Calculate the width of each color band
+  var numColors = palette.length;
+  var bandWidth = legendCanvas.width / numColors;
+
+  // Draw the color gradient
+  for (var i = 0; i < numColors; i++) {
+    ctx.fillStyle = palette[i];
+    ctx.fillRect(i * bandWidth, 0, bandWidth, legendCanvas.height);
+  }
+
+  // Update the legend min and max values
+  document.getElementById('legend-min').textContent = minValue;
+  document.getElementById('legend-max').textContent = maxValue;
+
+  document.getElementById('legend-min').textContent = minValue;
+  document.getElementById('legend-max').textContent = maxValue;
+}
+
+$(document).ready(function() {
+    // Initializers for date pickers with added triggers for time series index updates
+    $("#start-date, #end-date").datepicker({
+        dateFormat: "yy-mm-dd",
+        onSelect: function() {
+            let selectedGas = $('#gas-select').val();
+            test(selectedGas);
+            timeSeriesIndex(selectedGas); // Ensure this function is called with the correct gas
+        }
+    });
+
+    // Ensuring time series index is updated on gas selection changes
+    $('#gas-select').change(function() {
+        updateDatePickerRange($(this).val());
+        test($(this).val());
+        timeSeriesIndex($(this).val());
+    });
+});
+
+function updateDatePickerRange(selectedGas) {
+    var minDate, maxDate = new Date(); // maxDate is today for all gases.
+    switch(selectedGas) {
+        case "SO2":
+            minDate = new Date(2018, 6, 10);
+            break;
+        case "NO2":
+        case "CO":
+            minDate = new Date(2018, 5, 28);
+            break;
+        case "HCHO":
+            minDate = new Date(2018, 9, 2);
+            break;
+        case "O3":
+            minDate = new Date(2018, 6, 10);
+            break;
+        case "CH4":
+            minDate = new Date(2019, 1, 8);
+            break;
+        default:
+            minDate = new Date(2018, 5, 28); // Default min date if gas is unselected or unknown.
+    }
+
+    $("#start-date, #end-date").datepicker('option', 'minDate', minDate);
+    $("#start-date, #end-date").datepicker('option', 'maxDate', maxDate);
+}
+
+// Initialize the vector source and layer for markers
+// Define the icon style with a smaller scale for the marker
+var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: '/static/imgs/dot.svg', // Ensure this is a valid path to a marker image
+        scale: 0.03 // Smaller scale for the marker icon
+    })
+});
+
+// Initialize the vector source
+var vectorSource = new ol.source.Vector({});
+
+// Initialize the vector layer with the icon style that has a smaller scale
+var vectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+    style: iconStyle // Apply the style with a smaller icon scale
+});
+// Function to load station markers from OpenAQ API and add them to the map
+function loadStationsAndMarkers() {
+    $.ajax({
+        url: "https://api.openaq.org/v1/locations?country=AE",
+        type: "GET",
+        success: function(response) {
+            response.results.forEach(station => {
+                addMarker(station.coordinates.latitude, station.coordinates.longitude, station.location);
+            });
+            // Add the vector layer to the map only after all markers are added
+            map.addLayer(vectorLayer);
+        },
+        error: function() {
+            console.error('Failed to fetch station data');
+        }
+    });
+}
+
+// Function to add markers to the vector source with the smaller icon scale
+function addMarker(lat, lon, title) {
+    var iconFeature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([lon, lat])),
+        name: title
+    });
+
+    iconFeature.setStyle(iconStyle); // Apply the style with the smaller icon scale
+    vectorSource.addFeature(iconFeature);
+
+    iconFeature.setId(title); // Setting an ID for the feature for easy retrieval
+}
+
+
+
+// Ensuring the stations and markers are loaded after the map is fully initialized
+document.addEventListener("DOMContentLoaded", function() {
+    loadStationsAndMarkers(); // Make sure this is called after the map is ready
+});
+
+
+ 
+// Function to update the chart based on date selection
+// Initial chart creation
+function timeSeriesIndex(selectedGas) {
+  const startDate = $('#start-date').val();
+  const endDate = $('#end-date').val();
+
+  $.ajax({
+    url: api_url + 'timeSeriesIndex',
+    type: "POST",
+    async: true,
+    crossDomain: true,
+    contentType: "application/json",
+    data: JSON.stringify({ gas: selectedGas, startDate: startDate, endDate: endDate }),
+    success: function(data) {
+      if (data.timeseries) {
+          const ChartData = prepareChartData(data.timeseries);
+        // Now correctly passing the 'timeseries' data and a title for the chart
+          console.log("Timeseries data:", ChartData);
+        createChart(selectedGas, ChartData);
+      } else {
+        console.error('Expected timeseries data not found.');
+        console.log(data.errMsg || 'No error message provided');
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.error('Error fetching time series data:', textStatus, errorThrown);
+    }
+  });
+}
+function prepareChartData(rawData) {
+  return rawData
+    .map(item => {
+      // Check if the timestamp is in the expected format, if not, handle accordingly
+      const timestamp = item[0]; // assuming this is where the timestamp is in your data
+      let parsedDate;
+
+      // Handle different possible formats for the timestamp
+      if (typeof timestamp === 'string') {
+          console.log(timestamp);
+        // If the timestamp is a string, attempt to parse it
+        parsedDate = Date.parse(timestamp);
+      } else if (typeof timestamp === 'number') {
+        // If the timestamp is a number, it might already be in milliseconds
+        parsedDate = new Date(timestamp).getTime();
+      } else {
+        // If the timestamp is in an unexpected format, log an error or handle it as needed
+        console.error('Unexpected timestamp format:', timestamp);
+        parsedDate = NaN; // This will filter out the invalid data point later
+      }
+
+      const value = parseFloat(item[1]); // convert the value to a float
+      return [parsedDate, value];
+    })
+    .filter(item => !isNaN(item[0]) && !isNaN(item[1])); // filter out invalid data points
+}
+
+
