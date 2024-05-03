13fcc6bba43e7c9430262b1deac36128ebe5376f
Ali Badran <ali.badran53@yahoo.com>
2024-05-04 01:50:16 +0400

    changing port to allow webserver to access

--- static/js/map_utils.js
+++ static/js/map_utils.js
@@ -1,5 +1,5 @@
 let map;
-const api_url = "red-cliff-eb0f2586f97a41b0b060af01f2b0123a.azurewebsites.net";
+const api_url = "red-cliff-eb0f2586f97a41b0b060af01f2b0123a.azurewebsites.net:8000";
 
 function loadMap(target, center, zoom) {
     const raster = new ol.layer.Tile({
